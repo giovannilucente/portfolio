cbdd2100a472e6a7f89f4a3f2b85dd47a90819eb
Giovanni Lucente <51077901+giovannilucente@users.noreply.github.com>
2025-02-23 17:42:50 +0100

    Update train.py

--- diffusion_model_trajectory_predictor/train.py
+++ diffusion_model_trajectory_predictor/train.py
@@ -1,5 +1,5 @@
 """
-Author: Giovanni Lucente
+Author: Giovanni Lucente, Marko Mizdrak
 
 This script trains a model using different modalities (standard supervised learning or diffusion) 
 to predict the trajectories of traffic participants for one second. The predicted trajectories are 
